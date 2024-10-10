a9137032bf67a0cb203b6bd804a9c128a53eba3a
uuupppsss <ulya.ch.06@mail.ru>
2024-10-10 14:39:07 +1000

    чем дальше в лес шкибиди доп ес ес

--- WebChtoto/Controllers/PersonalData.cs
+++ WebChtoto/Controllers/PersonalData.cs
@@ -9,10 +9,12 @@ namespace WebChtoto.Controllers
     {
         private static List<Passport> _passports = new List<Passport>();
         private static List<Snils> _snilses = new List<Snils>();
+
         public PersonalData()
         {
             
         }
+
         [HttpPost("CreatePassport")]
         public ActionResult CreatePassport(Passport passport)
         {
@@ -20,7 +22,6 @@ namespace WebChtoto.Controllers
                 return BadRequest("Invalid passport");
             _passports.Add(passport);
             return Ok(passport);
-            
         }
 
         [HttpPost("CreateSnils")]
@@ -44,6 +45,29 @@ namespace WebChtoto.Controllers
                 return NotFound("We don't know this MC");
             return find;
         }
+
+        [HttpPost("SendClaim")]
+        public ActionResult SendClaim(Passport passport)
+        {
+            if (passport == null)
+                return BadRequest("Invalid passport");
+            _passports.Remove(passport);
+            return Ok("Data deleted (no)");
+        }
+
+        [HttpPost("SearchSnils")]
+        public ActionResult<Snils> SearchSnils(Search search)
+        {
+            if (string.IsNullOrEmpty(search.FirstName) ||
+                string.IsNullOrEmpty(search.LastName))
+                return BadRequest("Firstname or Lastname are empty");
+            var find = _snilses.FirstOrDefault(s => s.FirstName.Equals(search.FirstName) &&
+            s.LastName.Equals(search.LastName));
+            if (find == null)
+                return NotFound("We don't know this MC");
+            return find;
+        }
+
         private bool IsSnilseValid(Snils snils)
         {
             if (snils.Number.Length != 11||string.IsNullOrEmpty(snils.LastName) || string.IsNullOrEmpty(snils.FirstName)||!int.TryParse(snils.Number, out _))
